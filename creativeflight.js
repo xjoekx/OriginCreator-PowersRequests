08878595054931f2339f8e0b8f5f4f5f60a6456b
Exzotic5485 <61587328+Exzotic5485@users.noreply.github.com>
2022-05-01 12:59:14 +0100

    Creative Flight Example

---
+++ creativeflight.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "Creative Flight",
+    category: "Movement",
+    description: "Able to fly like creative mode",
+    json: `
+{
+	"type": "origins:creative_flight"
+}`
+}
