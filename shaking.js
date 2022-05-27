b5de584229729c0daad377eb6b3e38b5ad48279c
xjoekx <88422494+xjoekx@users.noreply.github.com>
2022-05-27 08:32:34 +0200

    Create shaking.js

---
+++ shaking.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: "Shaking",
+	category: "Miscellaneous",
+	description: "Shakes the Player, similar to a Strider out of Lava",
+	json: `{
+    "type": "origins:shaking"
+     }`
+}
