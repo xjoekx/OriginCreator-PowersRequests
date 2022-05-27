8843fb8b5f148e9e2b1a0206112ee4f0c8621a9b
xjoekx <88422494+xjoekx@users.noreply.github.com>
2022-05-27 09:03:14 +0200

    Update shaking.js

--- shaking.js
+++ shaking.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: "Shaking",
-	category: "Miscellaneous",
+	category: "Player",
 	description: "Shakes the Player, similar to a Strider out of Lava",
 	json: `{
     "type": "origins:shaking"
