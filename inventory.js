5cd2f685d2dc31ff889a5fa576953f921a575421
Exzotic5485 <61587328+Exzotic5485@users.noreply.github.com>
2022-06-02 21:11:39 +0100

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -1,25 +1,26 @@
 module.exports = {
-	name: "Inventory",
-	category: "Miscellaneous",
-	description: "Gives the Player a Shulk-like Inventory",
-	options: [{
-		title: "Title",
-		question: "What Name should the Inventory itself have ?",
-		placeholder: "{title}"
-	},{
-		title: "Key",
-		question: "What Key should the Inventory use ?",
-		placeholder: "{key}",
-		example: "primary , secondary"
-	},
-  },{
-		title: "Drops",
-		question: "Should the Inventory drop on death ?",
-		placeholder: "{drop}",
-	  	example: "true , false"
-	}
-  ],
-	json: `{
+    name: "Inventory",
+    category: "Miscellaneous",
+    description: "Gives the Player a Shulk-like Inventory",
+    options: [{
+            title: "Title",
+            question: "What Name should the Inventory itself have ?",
+            placeholder: "{title}"
+        }, 
+	{
+            title: "Key",
+            question: "What Key should the Inventory use ?",
+            placeholder: "{key}",
+            example: "primary , secondary"
+        },
+        {
+            title: "Drops",
+            question: "Should the Inventory drop on death ?",
+            placeholder: "{drop}",
+            example: "true , false"
+        }
+    ],
+    json: `{
         "name": "{name}",
         "description": "{description}",
         "type": "origins:inventory",
