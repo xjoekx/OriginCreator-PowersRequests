46c0533750e7e35a9b6f24b36ba7407092055bc6
Exzotic5485 <61587328+Exzotic5485@users.noreply.github.com>
2022-06-02 21:09:48 +0100

    Update ignore_water.js

--- ignore_water.js
+++ ignore_water.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: "Ignore Water",
 	category: "Movement",
-	description: "You can walk underwater, but not swim"
+	description: "You can walk underwater, but not swim",
 	json: `{
     "type": "origins:ignore_water"
   }`
