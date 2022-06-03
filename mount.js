f7fab8db7b41694f8d9ab528d8654a6bdba6585a
xjoekx <88422494+xjoekx@users.noreply.github.com>
2022-06-03 09:42:59 +0200

    Update mount.js

--- mount.js
+++ mount.js
@@ -3,6 +3,8 @@ module.exports = {
 	category: "Movement",
 	description: "Allows other Players to Sit on the Player",
 	json: `{
+	"name": "{name}",
+        "description": "{description}",
     "type": "origins:action_on_being_used",
     "bientity_action": {
         "type": "origins:mount"
