1afc6634209a613326b53cec4bf4d392a5bc3d3b
xjoekx <88422494+xjoekx@users.noreply.github.com>
2022-06-03 09:39:23 +0200

    Update effect_immunity.js

--- effect_immunity.js
+++ effect_immunity.js
@@ -9,6 +9,8 @@ module.exports = {
     example: "weakness , speed , etc."
 	}],
 	json: `{
+	"name": "{name}",
+        "description": "{description}",
     "type": "origins:effect_immunity",
     "effects": [
         "minecraft:{effect}"
