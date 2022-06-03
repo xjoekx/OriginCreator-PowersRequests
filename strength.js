9091dcfcbf514b0db24c731242a35994a800dbd0
xjoekx <88422494+xjoekx@users.noreply.github.com>
2022-06-03 09:37:05 +0200

    Update strength.js

--- strength.js
+++ strength.js
@@ -5,9 +5,9 @@ module.exports = {
     options: [
         {
             title: "Attack Strength",
-            question: "How much damage do you want to do? _(-2 = 1 less Heart, 0 = No Change, 2 = 1 more Heart)_",
-            placeholder: "{damage_delt}",
-            example: "0",
+            question: "How much damage do you want to do?",
+            placeholder: "{damage_dealt}",
+            example: "-2 = 1 less Heart, 0 = No Change, 2 = 1 more Heart",
         },
     ],
     json: `{
@@ -16,8 +16,8 @@ module.exports = {
     "type":"origins:attribute",
     "modifier":{
        "attribute":"minecraft:generic.attack_damage",
-       "value":{damage_delt},
+       "value":{damage_dealt},
        "operation":"addition"
     }
-}`
+  }`
 }
