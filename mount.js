1dbdf2ae4ecfcdc7f99503ee93eb2f76371acebd
xjoekx <88422494+xjoekx@users.noreply.github.com>
2022-06-03 09:31:51 +0200

    Create mount.js

---
+++ mount.js
@@ -0,0 +1,18 @@
+module.exports = {
+	name: "Mount",
+	category: "Movement",
+	description: "Allows other Players to Sit on the Player",
+	json: `{
+    "type": "origins:action_on_being_used",
+    "bientity_action": {
+        "type": "origins:mount"
+    },
+    "bientity_condition": {
+        "type": "origins:target_condition",
+        "condition": {
+            "type": "origins:passenger",
+            "inverted": true
+      }
+    }
+  }`
+}
